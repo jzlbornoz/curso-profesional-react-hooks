f18489b8aa3f4e15f57d95bcb05772faee9fdad3
Javier Albornoz <javieraap10@gmail.com>
2022-02-21 11:58:37 -0400

    Se agrego el context en la pagina Checkout, gracias a esto se pudo conenctar los articulos agregados al cart e  implementar las funciones: HandleRemove que permite eliminar los articulos y el HandleSumTotal que permite calcular el precio total de la compra

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
 import '../style/components/Checkout.css'
 
 const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleRemove = product => {
+    removeFromCart(product);
+  }
+
+  const HandleSumTotal = () => {
+    const reducer = (acumulator, currentValue) => (
+      acumulator + currentValue.price
+    );
+    const sum = cart.reduce(reducer, 0)
+    return sum
+  }
+
   return (
     <div className="Checkout">
       <div className="Checkout-content">
-        <h3>Lista de pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>Product Name</h4>
-            <span>$124</span>
+        {cart.length > 0 ? <h3>Lista de pedidos:</h3> :
+          <div className="Checkout-empty">
+            <h3 className='empty-h3'>Sin pedidos</h3>
+            <i className="fa-solid fa-triangle-exclamation fa-2xl" title='Sin pedidos' />
+          </div>}
+        {cart.map((item) => (
+          <div className="Checkout-item" >
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>{item.price}</span>
+            </div>
+            <button type='button' className='Checkout-delete' onClick={() => handleRemove(item)}>
+              <i className="fa-regular fa-trash-can fa-xl" />
+            </button>
           </div>
-          <button type='button' className='Checkout-delete'>
-            <i className="fa-regular fa-trash-can fa-xl" />
+        ))}
+      </div>
+      {cart.length > 0 && (
+        <div className="Checkout-sidebar">
+          <h2>{`Precio total: $ ${HandleSumTotal()}`}</h2>
+          <button type='button'>
+            <Link to='/checkout/information'>Continuar</Link>
           </button>
         </div>
-      </div>
-      <div className="Checkout-sidebar">
-        <h2>Precio total: $124</h2>
-        <button type='button'>
-          <Link to='/checkout/information'>Continuar</Link>
-        </button>
-      </div>
+      )}
     </div>
   )
 };
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -4,16 +4,16 @@ import initialState from '../initialState';
 const useInitialState = () => {
     const [state, setState] = useState(initialState);
 
-    const addToCart = (payload) => {
+    const addToCart = payload => {
         setState({
             ...state,
             cart: [...state.cart, payload]
         });
     };
-    const removeFromCart = (payload) => {
+    const removeFromCart = payload => {
         setState({
             ...state,
-            cart: [state.cart.filter(items => items.id !== payload.id)]
+            cart: state.cart.filter(items => items.id !== payload.id),
         });
     };
     return{
